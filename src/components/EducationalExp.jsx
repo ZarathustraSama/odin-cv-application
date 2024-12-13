54b80c43c0f545601209fdc407d0ef19ea30a45c
ZarathustraSama <mr.gabriel@live.it>
2024-12-13 17:15:11 +0100

    Refactored some code, implemented the same state/props logic from GeneralInfo

--- src/components/EducationalExp.jsx
+++ src/components/EducationalExp.jsx
@@ -1,44 +1,82 @@
 import { useState } from "react"
-import EditButton from "./EditButton";
-
-function SchoolNameInput({ name }) {
-  const [schoolName, setSchoolName] = useState(name);
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-function TitleInput({ title }) {
-  const [edTitle, setEdTitle] = useState(title);
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-function StudyDateInput({ date }) {
-  const [studyDate, setStudyDate] = useState(date);
-
-  return (
-    <>
-
-    </>
-  )
-}
+import Input from "./Input";
 
 
-export default function EducationalExp(props) {
-  return (
-    <>
-      <SchoolNameInput></SchoolNameInput>
-      <TitleInput></TitleInput>
-      <StudyDateInput></StudyDateInput>
-      <EditButton></EditButton>
-    </>
-  )  
+export default function EducationalExp() {
+  const [readOnly, setReadOnly] = useState(false);
+  const [schoolName, setSchoolName] = useState('');
+  const [title, setTitle] = useState('');
+  const [studyDate, setStudyDate] = useState('');
+
+  function handleSchoolNameChange(e) {
+    setSchoolName(e.target.value);
+  }
+
+  function handleTitleChange(e) {
+    setTitle(e.target.value);
+  }
+
+  function handleStudyDateChange(e) {
+    setStudyDate(e.target.value);
+  }
+
+  function handleClick(read) {
+    setReadOnly(read);
+  }
+
+  if (readOnly) {
+    return (
+      <section>
+        <div className="detail">
+          <label>School Name: </label>
+          <p>{schoolName}</p>
+        </div>
+        <div className="detail">
+          <label>Title: </label>
+          <p>{title}</p>
+        </div>
+        <div className="detail">
+          <label>Study Date: </label>
+          <p>{studyDate}</p>
+        </div>
+        <button
+          type="button"
+          className="edit"
+          onClick={ () => { handleClick(false) } }
+        >
+          Edit
+        </button>
+      </section>
+    )
+  }
+  else {
+    return (
+      <form onSubmit={e => e.preventDefault()}>
+        <Input
+          label={'School Name: '}
+          type={'text'}
+          value={schoolName}
+          onChange={handleSchoolNameChange}
+        ></Input>
+        <Input
+          label={'Title: '}
+          type={'text'}
+          value={title}
+          onChange={handleTitleChange}
+        ></Input>
+        <Input
+          label={'Study Date: '}
+          type={'date'}
+          value={studyDate}
+          onChange={handleStudyDateChange}
+        ></Input>
+        <button 
+          className="submit" 
+          onClick={ () => { handleClick(true) } }
+        >
+          Submit
+        </button>
+      </form>
+    )  
+  }
 }
