b0ba27d462d3fec9b6265ed1d05722676f72709f
ZarathustraSama <mr.gabriel@live.it>
2024-12-13 17:42:22 +0100

    Refactored some code, implemented the same state/props logic from GeneralInfo

--- src/components/PracticalExp.jsx
+++ src/components/PracticalExp.jsx
@@ -1,55 +1,97 @@
 import { useState } from "react"
-import EditButton from "./EditButton";
+import Input from "./Input";
 
-function CompanyNameInput({ name }) {
-  const [companyName, setCompanyName] = useState(name);
 
-  return (
-    <>
+export default function PracticalExp() {
+  const [readOnly, setReadOnly] = useState(false);
+  const [companyName, setCompanyName] = useState('');
+  const [positionTitle, setPositionTitle] = useState('');
+  const [jobResp, setJobResp] = useState('');
+  const [fromDate, setFromDate] = useState('');
+  const [untilDate, setUntilDate] = useState('');
 
-    </>
-  )
-}
+  function handleChange(setFunction, e) {
+    setFunction(e.target.value);
+  }
 
-function PositionTitleInput({ title }) {
-  const [positionTitle, setPositionTitle] = useState(title);
+  function handleClick(read) {
+    setReadOnly(read)
+  }
 
-  return (
-    <>
-
-    </>
-  )
-}
-
-function JobResponsibilitiesInput({ description }) {
-  const [respDescription, setRespDescription] = useState(description);
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-function FromUntilDatesInput({ from, until }) {
-  const [fromDate, setFromDate] = useState(from);
-  const [untilDate, setUntilDate] = useState(until);
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default function PracticalExp(props) {
-  return (
-    <>
-      <CompanyNameInput></CompanyNameInput>
-      <PositionTitleInput></PositionTitleInput>
-      <JobResponsibilitiesInput></JobResponsibilitiesInput>
-      <FromUntilDatesInput></FromUntilDatesInput>
-      <EditButton></EditButton>
-    </>
-  )
+  if (readOnly) {
+    return (
+      <section>
+        <div className="detail">
+          <label>Company Name: </label>
+          <p>{companyName}</p>
+        </div>
+        <div className="detail">
+          <label>Position Title: </label>
+          <p>{positionTitle}</p>
+        </div>
+        <div className="detail">
+          <label>Job Responsibilities: </label>
+          <p>{jobResp}</p>
+        </div>
+        <div className="detail">
+          <label>From: </label>
+          <p>{fromDate}</p>
+        </div>
+        <div className="detail">
+          <label>Until: </label>
+          <p>{untilDate}</p>
+        </div>
+        <button 
+          type="button" 
+          className="edit" 
+          onClick={ () => handleClick(false) }
+        >
+          Edit
+        </button>
+      </section>
+    )
+  }
+  else {
+    return (
+      <form onSubmit={e => e.preventDefault()}>
+        <Input
+          label={'Company Name: '}
+          type={'text'}
+          value={companyName}
+          onChange={e => { handleChange(setCompanyName, e) }}
+        ></Input>
+        <Input
+          label={'Position Title: '}
+          type={'text'}
+          value={positionTitle}
+          onChange={e => { handleChange(setPositionTitle, e) }}
+        ></Input>
+        <Input
+          label={'Job Responsibilities: '}
+          type={'text'}
+          value={jobResp}
+          onChange={e => { handleChange(setJobResp, e) }}
+        ></Input>
+        <Input
+          label={'From: '}
+          type={'date'}
+          value={fromDate}
+          onChange={e => { handleChange(setFromDate, e) }}
+        ></Input>
+        <Input
+          label={'Until: '}
+          type={'date'}
+          value={untilDate}
+          onChange={e => { handleChange(setUntilDate, e) }}
+        ></Input>
+        <button
+          type="button" 
+          className="submit" 
+          onClick={ () => { handleClick(true) } }
+        >
+          Submit
+        </button>
+      </form>
+    )
+  }
 }
